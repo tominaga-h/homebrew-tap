d106d15e89e2bbee7f0dd7b1f10fee4163830f07
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-03-10 22:49:08 +0900

    mdc v2.0.0 released

--- Formula/mdc.rb
+++ Formula/mdc.rb
@@ -1,12 +1,12 @@
 class Mdc < Formula
   desc "A CLI tool to orchestrate multi-repository Docker environments"
   homepage "https://github.com/tominaga-h/mdc"
-  version "1.0.0"
+  version "2.0.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/tominaga-h/multi-docker-commander/releases/download/v1.0.0/mdc"
-    sha256 "de278d44628fe3e1d55e6fe2be2a6e1bb393f9bf0e584d04ed4ba414782c3453"
+    url "https://github.com/tominaga-h/multi-docker-commander/releases/download/v2.0.0/mdc"
+    sha256 "91beb510ff8bfa50e28c128f5ddc826bf2ea89336a0d4a924793c11244d136d9"
   end
 
   def install
