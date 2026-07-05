2cd506782f2e9bca02834da2231c4532ddeb7beb
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-07-05 16:01:16 +0900

    Bump jarvish to v1.13.3

--- Formula/jarvish.rb
+++ Formula/jarvish.rb
@@ -1,12 +1,12 @@
 class Jarvish < Formula
   desc "Next Generation AI Integrated Shell inspired by J.A.R.V.I.S."
   homepage "https://github.com/tominaga-h/jarvis-shell"
-  version "1.13.1"
+  version "1.13.3"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     url "https://github.com/tominaga-h/jarvis-shell/releases/download/v#{version}/jarvish-aarch64-apple-darwin.tar.gz"
-    sha256 "ad8b7f135579d3fbe36ecb993b475c80b9f7092bb348e6f692016a8f0ab202c8"
+    sha256 "666e49ac99e97376f015581dc8b78e0ee8776ef25f08782eef989161fb61be4d"
   end
 
   def install
